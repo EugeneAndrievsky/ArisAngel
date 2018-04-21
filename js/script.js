799bbeb98fc6e82387183e5f6f3f345abed82f45
EugeneAndrievsky <windbojik@yandex.ru>
2018-04-21 19:45:07 +0300

    Поправил отображение стихов

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(function(){
   var $verses = $('.poem__content');
+  var flag = false;
+  var setDelay = function() {
+    if (flag) {
+      return 500;
+    }
+    return 0;
+  }
 
   $('header').fadeIn(2000);
   $('.main-nav__list').on('click', '.main-nav__link', function(event) {
@@ -7,12 +14,14 @@ $(function(){
     var url = this.href;
     var $this = $(this);
 
+// Визуальное переключение страниц в навигации
     $this
       .closest('.main-nav__list')
       .find('.main-nav__link')
       .removeClass('main-nav__link--current');
     $this.addClass('main-nav__link--current');
 
+// Подгрузка страниц через ajax
     $('#content').remove();
     $('.main-content').load(url + ' #content', function(){
       $('.poem__content').hide();
@@ -21,17 +30,25 @@ $(function(){
       .fadeIn('slow');
   });
 
-
-  $('.main-content').on('click', '.poem__name', function() {
+// Показ стихов
+  $('.main-content').on('click', '.poem__title', function() {
+    var $this = $(this);
     var $nextVerses = $(this).next($verses);
-    if($nextVerses.hasClass('.poem__name--opened')) {
+
+    if(!$this.hasClass('poem__title--current')) {
+      $('.poem__title--current').removeClass('poem__title--current');
+      $('.poem__content--opened').slideUp(500);
+      $this.addClass('poem__title--current');
       $nextVerses
-        .removeClass('.poem__name--opened')
-        .slideUp(500);
-    } else {
-      $nextVerses
-        .addClass('.poem__name--opened')
+        .delay(setDelay())
+        .addClass('poem__content--opened')
         .slideDown(500);
+        flag = true;
+    } else {
+      $this.removeClass('poem__title--current');
+      $nextVerses
+        .removeClass('poem__content--opened')
+        .slideUp(500);
     }
   });
 
